2298d46d81cfaaebae56c5381cdc931345246e78
Arthur Santana Rodrigues Castilhos <arthursantanarodrigues2006@gmail.com>
2025-04-24 22:14:43 -0300

    terminado eu acho

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,27 +1,25 @@
- //chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
+//chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
 
- 
- // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import { getFirestore } from  "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAEl48Do1c_aLUzhyOiT-JApIuMn34Tons",
-    authDomain: "teste-b28fe.firebaseapp.com",
-    databaseURL: "https://teste-b28fe-default-rtdb.firebaseio.com",
-    projectId: "teste-b28fe",
-    storageBucket: "teste-b28fe.firebasestorage.app",
-    messagingSenderId: "240592542465",
-    appId: "1:240592542465:web:8a94c735325968c78e1900",
-    measurementId: "G-S2BJ6B6JCX"
-  };
+// Configuração correta do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAEl48Do1c_aLUzhyOiT-JApIuMn34Tons",
+  authDomain: "teste-b28fe.firebaseapp.com",
+  databaseURL: "https://teste-b28fe-default-rtdb.firebaseio.com",
+  projectId: "teste-b28fe",
+  storageBucket: "teste-b28fe.firebasestorage.app",
+  messagingSenderId: "240592542465",
+  appId: "1:240592542465:web:8a94c735325968c78e1900",
+  measurementId: "G-S2BJ6B6JCX"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  
-  const db = getFirestore(app);
-  export {db,app};
+
+// Inicializa o app
+const app = initializeApp(firebaseConfig);
+
+// Inicializa e exporta o Firestore
+const db = getFirestore(app);
+export { db, app };
--- script.js
+++ script.js
@@ -1,33 +1,139 @@
-import { db } from "./firebaseConfig.js";
-import {collection,addDoc} from "https://gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { db } from './firebaseConfig.js ';
+import { collection, addDoc, getDocs,doc,deleteDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
+
+// SALVANDO DADOS NO BANCO 
 function getInputs() {
-  return {
-    nome: document.getElementById("nome"),
-    idade: document.getElementById("idade"),
-    cargo: document.getElementById("cargo")
-  };
-}
-  function getValores({nome, idade, cargo}) {
     return {
-      nome: nome.value.trin(),
-      idade: parseInt(idade.value),
-      cargo: cargo.value.trin()
+        nome: document.getElementById('nome'),
+        idade: document.getElementById('idade'),
+        cargo: document.getElementById('cargo')
     };
+}
+
+function getValores({ nome, idade, cargo }) {
+    return {
+        nome: nome.value.trim(),
+        idade: parseInt(idade.value),
+        cargo: cargo.value.trim()
+    };
+}
+
+function limpar({nome, idade, cargo}){
+    nome.value = ''
+    idade.value = ''
+    cargo.value = ''
+}
+
+document.getElementById("btnEnviar").addEventListener('click', async function(){
+    const Inputs = getInputs(); 
+    const dados =getValores(Inputs)
+
+    console.log("Inputs:", Inputs)
+    console.log("Dados", dados)
+
+    if (!dados.idade || !dados.nome || !dados.cargo){
+        alert("Preencha todos os campos.");
+        return
+    } 
+    try{
+        const ref = await addDoc(collection(db, "funcionarios"), dados);
+        console.log("ID do documento", ref.id); 
+        limpar(Inputs)
+        alert("Funcionário cadastrado com sucesso: ")
+    }catch (e){
+        console.log("Erro: ", e)
+    }
+});
+
+// CONSULTAR (LISTAR) OS DADOS DO BANCO
+
+   
+
+async function buscarFuncionarios() {
+    const dadosBanco = await getDocs(collection(db, "funcionarios"));
+    console.log(dadosBanco)
+    const funcionarios = []; 
+    for (let funcionario of dadosBanco.docs){
+        funcionarios.push({id: funcionario.id, ...funcionario.data() });
+    }
+    console.log(funcionarios)
+    return funcionarios
+}
+
+const listaFuncionariosDiv = document.getElementById("listar-funcionarios")
+
+async function carregarListaDeFuncionarios() {
+    listaFuncionariosDiv.innerHTML = '<p> Carregando lista de Funcionarios... </p>';
+    try {
+        let funcionario = await buscarFuncionarios();
+        console.log(funcionario)
+        renderizarListaDeFuncionarios(funcionario);
+    } catch (error) {
+        console.log("Erro ao carregar a lista de funcionarios: ", error);
+        listaFuncionariosDiv.innerHTML = '<p> Erro ao carregar a lista de funcionários... </p>';
+    }
+}
+
+function renderizarListaDeFuncionarios(funcionarios){
+    listaFuncionariosDiv.innerHTML= '';
+
+    if (funcionarios.length === 0){
+        listaFuncionariosDiv.innerHTML = '<p> Nenhum Funcionário cadastrado ainda ;( </p> ';
+        return 
+    }
+    for (let funcionario of funcionarios) {
+        const funcionarioDiv = document.createElement('div');
+        funcionarioDiv.innerHTML = `
+            <strong> Nome: </strong> ${funcionario.nome} <br>
+            <strong> Idade: </strong> ${funcionario.idade} <br>
+            <strong> Cargo: </strong> ${funcionario.cargo} <br>
+            <button class = "btn-excluir " data-id= "${funcionario.id}" > excluir </button>
+            <hr>
+        `
+        listaFuncionariosDiv.appendChild(funcionarioDiv);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', carregarListaDeFuncionarios);
+
+//EXCLUIR DADOS DO BANCO DE DADOS 
+
+async function excluirFuncionario(idfuncionario) {
+  try{
+    const documentoDeletar = doc(db, 'funcionarios', idfuncionario);
+    await deleteDoc (documentoDeletar);
+    console.log("funcionario com ID" + idfuncionario + 'foi excluido');
+  return true;
+  }catch (erro){
+    console.log("Erro ao excluir o funcionario", erro);
+    alert("Ocorreu um erro ao excluir funcionario. Tente novamente");
+    return false;
+   }
+}
+
+async function lidarclique(eventoDeClique) {
+  const btnexcluir = eventoDeClique.target.closest('.btn-excluir');
+  const certeza = confirm("Tem certeza que deseja fazer essa exclusão");
+if(certeza){
+
+
+
+  if (btnexcluir){
+  const idfuncionario = btnexcluir.dataset.id;
+  const exclusaoBemSucedida = await excluirFuncionario (idfuncionario);
+ 
+
+  if(exclusaoBemSucedida){
+    carregarListaDeFuncionarios();
+    alert('Funcionario excluido com sucesso!');
+   }
   }
 
-document.getElementById("botaoEnviar").addEventListener("click", async function(){
-  const Inputs = getInputs();
-  const dados = getValores (Inputs);
-  console.log('Inputs:',Inputs) 
-  console.log('Dados',dados)
- 
-  try{
-  const ref =  await addDoc (collection(db, "funcionarios"),dados);
-  console.log("ID do documento", ref.id);
-  }
-  catch(e){
-  console.log("Error",e)
- }
-});
-document.addEventListener()
+}else{
+  alert("Exclusao cancelada");
+}
+  return
+}
+
+listaFuncionariosDiv.addEventListener('click', lidarclique);
